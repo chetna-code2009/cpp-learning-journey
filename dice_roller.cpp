296c4889d8de7cb7665505389c617dbee1ebc147
chetna-code2009 <chetnarawar974@gmail.com>
2026-08-08 17:00:52 +0530

    Add first C++ dice roller  program

--- dice_roller.cpp
+++ dice_roller.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int main() {
+    srand(time(0));
+
+    int dice = rand() % 6 + 1;
+    cout << "You rolled a " << dice << endl;
+    return 0;
+}
